97b7727acab8708712e344104d2a19d922aab495
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-04-06 10:44:05 -0400

    commit tree c12b43f54edb1fc638c8dd3a44d8998bda007aae

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return(`This Saturday, I want to ${activity}!`)
+}
+
+const mondayWork = function(workStyle="go to the office") {
+    return (`This Monday, I will ${workStyle}.`)
+}
+
+function wrapAdjective(decor = "*") {
+    return function (descriptor = "special"){
+        return (`You are ${decor}${descriptor}${decor}!`)
+    }
+}
